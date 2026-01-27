64008fc3904a0516315c2b7aab497d2f78b29c55
WojciechXi <wojciech.ratuszynski@gmail.com>
2026-01-27 15:51:24 +0100

    Update

--- Classes/Engine/2.Obj/Component/Collider/class.MeshCollider.js
+++ Classes/Engine/2.Obj/Component/Collider/class.MeshCollider.js
@@ -1,5 +1,108 @@
 class MeshCollider extends Collider {
 
+    Init() {
+        super.Init();
+        this.mesh = null;
+    }
 
+    OnEnable() {
+        const meshFilter = this.GetComponent(MeshFilter);
+        if (meshFilter) {
+            this.mesh = meshFilter.mesh;
+        }
+    }
+
+    get worldCenter() { return this.mesh ? this.transform.TransformPoint(this.mesh.bounds.center) : super.worldCenter; }
+    get localBounds() { return this.mesh ? new Bounds(this.mesh.bounds.center.Clone(), this.mesh.bounds.size.Clone()) : super.localBounds; }
+    get bounds() {
+        const hx = this.size.x / 2;
+        const hy = this.size.y / 2;
+        const hz = this.size.z / 2;
+
+        return GeometryUtility.CalculateBounds([
+            new Vector3(this.center.x + hx, this.center.y + hy, this.center.z + hz),
+            new Vector3(this.center.x + hx, this.center.y + hy, this.center.z - hz),
+            new Vector3(this.center.x + hx, this.center.y - hy, this.center.z + hz),
+            new Vector3(this.center.x + hx, this.center.y - hy, this.center.z - hz),
+            new Vector3(this.center.x - hx, this.center.y + hy, this.center.z + hz),
+            new Vector3(this.center.x - hx, this.center.y + hy, this.center.z - hz),
+            new Vector3(this.center.x - hx, this.center.y - hy, this.center.z + hz),
+            new Vector3(this.center.x - hx, this.center.y - hy, this.center.z - hz),
+        ], this.transform.matrix4x4);
+    }
+
+    Intersects(other) {
+        if (!other) return false;
+
+        if (other instanceof BoxCollider) return this.bounds.Intersects(other.bounds);
+        if (other instanceof SphereCollider) return this.bounds.Intersects(other.bounds);
+        if (other instanceof CylinderCollider) return this.bounds.Intersects(other.bounds);
+        if (other instanceof CapsuleCollider) return this.bounds.Intersects(other.bounds);
+
+        return false;
+    }
+
+    Raycast(ray, maxDistance) {
+        if (!this.mesh) return null;
+
+        const localRay = this.transform.InverseTransformRay(ray);
+
+        let distance = Infinity;
+        let nearestNormal = null;
+
+        const vertices = this.mesh.vertices;
+        const triangles = this.mesh.subMesh[0].triangles;
+
+        // Iteracja przez trójkąty (co 3 indeksy)
+        for (let i = 0; i < triangles.length; i += 3) {
+            const v0 = vertices[triangles[i]];
+            const v1 = vertices[triangles[i + 1]];
+            const v2 = vertices[triangles[i + 2]];
+
+            const t = this.IntersectTriangle(localRay, v0, v1, v2);
+
+            if (t !== null && t < distance && t <= maxDistance) {
+                distance = t;
+
+                const edge1 = Vector3.Subtract(v1, v0);
+                const edge2 = Vector3.Subtract(v2, v0);
+                nearestNormal = Vector3.Cross(edge1, edge2).normalize();
+            }
+        }
+
+        if (distance === Infinity) return null;
+
+        const localPoint = localRay.GetPoint(distance);
+
+        return new RaycastHit(
+            this,
+            this.transform.TransformPoint(localPoint),
+            this.transform.TransformDirection(nearestNormal),
+            distance
+        );
+    }
+
+    IntersectTriangle(ray, v0, v1, v2) {
+        const edge1 = Vector3.Subtract(v1, v0);
+        const edge2 = Vector3.Subtract(v2, v0);
+        const h = Vector3.Cross(ray.direction, edge2);
+        const a = Vector3.Dot(edge1, h);
+
+        if (a > -0.00001 && a < 0.00001) return null; // Promień równoległy
+
+        const f = 1.0 / a;
+        const s = Vector3.Subtract(ray.origin, v0);
+        const u = f * Vector3.Dot(s, h);
+
+        if (u < 0.0 || u > 1.0) return null;
+
+        const q = Vector3.Cross(s, edge1);
+        const v = f * Vector3.Dot(ray.direction, q);
+
+        if (v < 0.0 || u + v > 1.0) return null;
+
+        const t = f * Vector3.Dot(edge2, q);
+        return t > 0.00001 ? t : null;
+    }
 
 }
---
+++ Classes/Engine/4.Physics/Geometry/class.TriangleGeometry.js
@@ -0,0 +1,11 @@
+class TriangleGeometry {
+
+
+
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+}
